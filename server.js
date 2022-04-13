f5436dd28badf0d820b21603990f66919fc73c09
zand-yasin <zand.kurdy@gmail.com>
2022-04-13 14:56:30 +0300

    user auth

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorFounder } from './middleware/errorMiddleware.js'
 dotenv.config()
 
 connectDB()
 const app = express()
 
+// allow us to accept json data from the body
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is working')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
