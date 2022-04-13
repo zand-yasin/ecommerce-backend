da244901cdfe86235e126798ea07afe15d52f7fc
zand-yasin <zand.kurdy@gmail.com>
2022-04-14 02:12:48 +0300

    user register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express'
 const router = express.Router()
 
-import { authUser, getUserProfile } from '../controllers/userControllers.js'
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 // same thing
 // router.route('/login').post(authUser)
 router.post('/login', authUser)
-
+router.post('/create', registerUser)
 router.route('/profile').get(protect, getUserProfile)
 
 export default router
