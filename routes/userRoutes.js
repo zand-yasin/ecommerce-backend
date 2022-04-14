a86b7ee329d47aabcd81337437ff041c9e41bfec
zand-yasin <zand.kurdy@gmail.com>
2022-04-14 11:49:42 +0300

    Add Update User

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,17 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 // same thing
 // router.route('/login').post(authUser)
 router.post('/login', authUser)
 router.post('/create', registerUser)
-router.route('/profile').get(protect, getUserProfile)
+// router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
