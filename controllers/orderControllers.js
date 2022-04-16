84c7eb8e1ac14bb2bc9d4ab0a213ad2ebf039ecc
zand-yasin <zand.kurdy@gmail.com>
2022-04-16 12:40:15 +0300

    update order to paid

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -52,7 +52,30 @@ const getOrderById = asyncHandler(async (req, res) => {
     res.status(404)
   }
 })
-export { addOrderItems, getOrderById }
+// @desc UPDATE order to paid
+// @route post /api/orders/:id/pay
+// @access Private
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    }
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+export { addOrderItems, getOrderById, updateOrderToPaid }
 
 // // @desc Fetch single product
 // // @route GET /api/product/:id
